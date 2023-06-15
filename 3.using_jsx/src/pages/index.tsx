25222e391d8f5bfe83ab99cdd862b74c2fd9f920
Zenkie Bear <zq@zenkie.cn>
2023-06-15 17:43:28 +0800

    ci: upload lesson 3

---
+++ 3.using_jsx/src/pages/index.tsx
@@ -0,0 +1,18 @@
+
+export default function TodoList() {
+  return (
+    <>
+      <h1>Zenkie Bear's Todolist</h1>
+      <img
+        src="https://i.imgur.com/yXOvdOSs.jpg" 
+        alt="Hedy Lamarr" 
+        className="photo" 
+      />
+      <ul>
+        <li>Learn react</li>
+        <li>Learn cpp</li>
+        <li>Start use kubernetes</li>
+      </ul>
+    </>
+  )
+}
