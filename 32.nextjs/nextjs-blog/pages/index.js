66ee4c8077a22558651fa52475bff5501ed79f98
Zenkie Bear <zq@zenkie.cn>
2023-08-31 15:57:10 +0800

    feat: finished nextjs basics

--- 32.nextjs/nextjs-blog/pages/index.js
+++ 32.nextjs/nextjs-blog/pages/index.js
@@ -27,18 +27,11 @@ export default function Home({ allPostsData }) {
         <h2 className={utilStyles.headingLg}>Blog</h2>
         <ul className={utilStyles.list}>
           {allPostsData.map(({id, date, title}) => (
-            // <li className={utilStyles.listItem} key={id}>
-            //   <Link href={`/posts/${id}`}>{title}</Link>
-            //   <br />
-            //   <small className={utilStyles.lightText}>
-            //     <Date dateString={date}/>
-            //   </small>
-            // </li>
             <li className={utilStyles.listItem} key={id}>
               <Link href={`/posts/${id}`}>{title}</Link>
               <br />
               <small className={utilStyles.lightText}>
-                <DateBox dateString={date} />
+                <DateBox dateString={date}/>
               </small>
             </li>
           ))}
