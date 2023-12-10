5fa1a1470932022edd8af05588622f989607a95c
Zenkie Bear <zq@zenkie.cn>
2023-12-10 14:07:29 +0800

    perf: optimized with list style

--- redux-essentials-example-app/src/features/posts/PostList.tsx
+++ redux-essentials-example-app/src/features/posts/PostList.tsx
@@ -2,6 +2,7 @@ import { useSelector } from "react-redux"
 import { RootState } from "app/store"
 import { Link } from "react-router-dom"
 import PostAuthor from "./PostAuthor"
+import TimeAgo from "./TimeAgo"
 
 export const PostsList = () => {
   const posts = useSelector((state: RootState) => state.posts)
@@ -9,13 +10,16 @@ export const PostsList = () => {
   const renderedPosts = posts.map((post: Post) => (
     <article className="post-excerpt" key={post.id}>
       <h3>
-        {post.title}
-        <PostAuthor id={post.userId} />
+        <div>
+          {post.title}
+          <PostAuthor id={post.userId} />
+        </div>
+        <TimeAgo timestamp={post.date} />
       </h3>
       <p className="post-content">
         {post.content.substring(0, 100)} {' '}
-        <Link to={`/posts/${post.id}`}>View</Link>
       </p>
+      <Link to={`/posts/${post.id}`}>View</Link>
     </article>
   ))
 
