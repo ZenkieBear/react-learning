0e260cf888f34471ba1e7cade2472313c4e0997b
Zenkie Bear <zq@zenkie.cn>
2023-12-10 11:30:15 +0800

    doc: changed navbar title

--- redux-essentials-example-app/src/app/Navbar.tsx
+++ redux-essentials-example-app/src/app/Navbar.tsx
@@ -1,11 +1,10 @@
-import React from 'react'
 import { Link } from 'react-router-dom'
 
 export const Navbar = () => {
   return (
     <nav>
       <section>
-        <h1>Redux Essentials Example</h1>
+        <h1>Bonjour! Redux essentials</h1>
 
         <div className="navContent">
           <div className="navLinks">
